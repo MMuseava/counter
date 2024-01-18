605c99dc01e753993c4c86ec81388affd81a8527
meerim <mmusaeva2023@gmail.com>
2024-01-18 13:27:18 -0600

    add calc button

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import ComponentTen from "./components/componentTen/ComponentTen";
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+	// const count = 0;
+	const [count, setCount] = useState(1);
+
+	const onMinusClick = () => {
+		if (count > 0) {
+			setCount(count - 1);
+		}
+	};
+
+	const onPlusClick = () => {
+		if (count < 10) {
+			setCount(count + 1);
+		}
+	};
+	const showMessage = () => {
+		if (count < 10 && count > 0) {
+			return <p>keep it going</p>;
+		} else if (count === 10) {
+			return <p>Reached Max!</p>;
+		} else if (!count) {
+			return <p>Reached Min!</p>;
+		}
+	};
+
+	return (
+		<div className="counter">
+			<div className="button-group">
+				<button onClick={onMinusClick}>-</button>
+				<p>{count}</p>
+				<button onClick={onPlusClick}>+</button>
+			</div>
+			<div>{showMessage()}</div>
+				<ComponentTen />
+			<div>
+				</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/componentTen/ComponentTen.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+ import "./componentTen.style.css"
+
+
+
+
+
+const ComponentTen = () => {
+
+    const [count, setCount] = useState(10)
+    const onMinusClick = () => {
+        if (count > 0) {
+            
+            setCount(count - 10)
+        }
+    
+}
+    const onPlusClick = () => {
+        if (count < 100) {
+            
+            setCount(count + 10)
+        }
+
+        
+    
+    }
+    
+    const showMessage = () => {
+		if (count < 100 && count > 10) {
+			return <p>keep it going</p>;
+		} else if (count === 100) {
+			return <p>Reached Max!</p>;
+		} else if (!count) {
+			return <p>Reached Min!</p>;
+		}
+	};
+  return (
+      <div className='ten'>
+          <div  className="button-group">
+          <button className='button-ten' onClick={onMinusClick}>*</button>
+          <p>
+              {count}
+          </p>
+              <button className='button-ten' onClick={onPlusClick}>/</button>
+              </div>
+          <div>{showMessage()}</div>
+
+    </div>
+  )
+}
+
+export default ComponentTen
